19f96cf107faf99f44fc0781b6ad811219ed27bc
Christian Torrisi <christian.torrisi@my.jcu.edu.au>
2021-09-06 22:31:03 +1000

    Attempt Exercise 4.1

---
+++ Practical02/src/Ex4_1.java
@@ -0,0 +1,34 @@
+// CP2406 Prac 2 - Christian Torrisi
+//Exercise 3.1 - Print Capitalised
+
+import java.util.Scanner;
+
+public class Ex4_1 {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println();
+        String sentence = userInput.nextLine();
+
+        System.out.println(printCapitalised(sentence));
+
+
+
+    }
+
+    private static String printCapitalised(String str) {
+        String capitalisedString = "";
+        String[] words = str.split("[,\\s\".]");
+
+        for (String word : words) {
+            if (word != "") {
+                // Capitalise the first letter at element 0 and then add the rest of the string to it.
+                word = Character.toUpperCase(word.charAt(0)) + word.substring(1);
+                capitalisedString += word + " ";
+            }
+        }
+        return capitalisedString;
+    }
+
+}
+
