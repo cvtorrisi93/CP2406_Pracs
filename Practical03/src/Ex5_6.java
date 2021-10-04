06ce24eba34c86488e53d377c81ad1503d9d4a77
Christian Torrisi <christian.torrisi@my.jcu.edu.au>
2021-10-04 18:14:23 +1000

    Create getResults function.

--- Practical03/src/Ex5_6.java
+++ Practical03/src/Ex5_6.java
@@ -1,6 +1,7 @@
 // CP2406 Prac 3 - Christian Torrisi
 //Exercise 5.6 - Addition Questionnaire
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ex5_6 {
@@ -15,7 +16,7 @@ public class Ex5_6 {
 
         createQuestions();
         answerQuestions();
-        //TODO getResults();
+        getResults();
     }
 
     private static void createQuestions() {
@@ -28,9 +29,28 @@ public class Ex5_6 {
     private static void answerQuestions() {
         Scanner userInput = new Scanner(System.in);
         userAnswers = new int[10];
+        int questionNumber = 0;
         for (int i = 0; i < 10; i++) {
-            System.out.println(questions[i].getQuestion());
+            questionNumber++;
+            System.out.println(questionNumber + ". " + questions[i].getQuestion());
             userAnswers[i] = userInput.nextInt();
         }
+    }
+    private static void getResults() {
+        System.out.println("Here are the correct answers!");
+        int correctCount = 0;
+        int questionNumber = 0;
+        for (int i = 0; i < 10; i++) {
+            questionNumber++;
+            System.out.println("Question " + questionNumber + ". Answer: " + questions[i].getCorrectAnswer());
+            if (userAnswers[i] == questions[i].getCorrectAnswer()) {
+                System.out.println("Correct!");
+                correctCount++;
+            } else {
+                System.out.println("Your answer " + userAnswers[i] + " was incorrect.");
+            }
+
+        }
+        System.out.println("You got " + correctCount + " out of 10 answers correct.");
     }
 }
